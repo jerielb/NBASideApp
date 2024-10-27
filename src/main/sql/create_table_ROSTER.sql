b8913fb9fb87782afebe38045850a0b004a08075
jerielB <jerielbongco98@gmail.com>
2024-10-27 15:19:52 +1100

    refactored create table files

---
+++ src/main/sql/create_table_ROSTER.sql
@@ -0,0 +1,9 @@
+---- foreign keys prevent from truncating a table ----
+-- drop table ROSTER;
+
+create table ROSTER (
+	TEAM_ID int, -- foreign key
+    PLAYER_ID int -- foreign key
+--    , foreign key(TEAM_ID) references TEAM(TEAM_ID),
+--    foreign key(PLAYER_ID) references PLAYER(PLAYER_ID)
+);
